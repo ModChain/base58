ff970d55c06b0d76cbf3dca246b1a58fbe45f00c
Mark Karpeles <magicaltux@gmail.com>
2025-03-02 10:05:55 +0900

    add more tests

--- chunked_test.go
+++ chunked_test.go
@@ -3,6 +3,7 @@ package base58_test
 import (
 	"bytes"
 	"encoding/hex"
+	"strings"
 	"testing"
 
 	"github.com/ModChain/base58"
@@ -14,6 +15,16 @@ func TestChunked(t *testing.T) {
 		"00000000",
 		"01234567abcbcdef",
 		"01234567abcbcdef01234567abcbcdef01234567abcbcdef01234567abcbcdef",
+		"0102030405060708090a0b0c0d0e0f101112131415161718191a1b1c1d1e1f",
+	}
+
+	// extra test vectors
+	for n := 0; n < 32; n++ {
+		vectors = append(vectors,
+			strings.Repeat("00", n),
+			strings.Repeat("ff", n),
+			strings.Repeat("55", n),
+		)
 	}
 
 	for _, vechex := range vectors {
